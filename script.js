3b99aff33cbc09b324275f989b46a5f13f8ce1b5
pyuser <enes.karakas.e@gmail.com>
2021-08-09 15:10:15 +0300

    v6 done, project done, password match, store data issue done

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
 let isValid = false;
+let passwordsMatch = false;
 
-function validateForm(e) {
-  e.preventDefault();
+function validateForm() {
   isValid = form.checkValidity();
 
   if (!isValid) {
@@ -17,6 +17,51 @@ function validateForm(e) {
     messageContainer.style.borderColor = "red";
     return;
   }
+
+  if (password1El.value !== password2El.value) {
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+
+    password1El.style.borderColor = "red";
+    password2El.style.borderColor = "red";
+  } else {
+    passwordsMatch = false;
+    message.textContent = "Make sure passwords match.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1El.style.borderColor = "red";
+    password2El.style.borderColor = "red";
+    return;
+  }
+
+  if (isValid && passwordsMatch) {
+    message.textContent = "Successfully Registered!";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+  }
 }
 
-form.addEventListener("submit");
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  // Do something with user data
+  console.log(user);
+}
+
+function processFormData(e) {
+  e.preventDefault();
+  // Validate Form
+  validateForm();
+  // Submit Form if Valid
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
+}
+
+form.addEventListener("submit", processFormData);
