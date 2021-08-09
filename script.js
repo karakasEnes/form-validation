2093fea7c52005d1e778cfb37b58f592e9cc7687
pyuser <enes.karakas.e@gmail.com>
2021-08-09 13:38:25 +0300

    validation are doen in js side, v5 is done

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
+
+let isValid = false;
+
+function validateForm(e) {
+  e.preventDefault();
+  isValid = form.checkValidity();
+
+  if (!isValid) {
+    // Style main message for an error
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
+  }
+}
+
+form.addEventListener("submit");
